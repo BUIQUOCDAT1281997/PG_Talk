fbaa82b842ff609ee8a725aeb9bec7695f3ce3bd
Bui Quoc Dat <buiquocdat1281997@gmail.com>
2020-03-13 22:23:23 +0300

    first commit

---
+++ app/src/main/java/quocdat/kurs/pgtalk/MainActivity.java
@@ -0,0 +1,13 @@
+package quocdat.kurs.pgtalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
